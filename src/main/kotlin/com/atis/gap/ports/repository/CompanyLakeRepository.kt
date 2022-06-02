3fcc62ab0f93a4146a0edb0295a66cf80659f3c8
Adriano Ferreira <afsp@MacBook-Pro-de-Adriano.local>
2022-06-02 00:55:09 -0300

    Ajustes no algoritmo

--- src/main/kotlin/com/atis/gap/ports/repository/CompanyLakeRepository.kt
+++ src/main/kotlin/com/atis/gap/ports/repository/CompanyLakeRepository.kt
@@ -1,6 +1,5 @@
 package com.atis.gap.ports.repository
 
 import com.atis.gap.application.domain.enterprise.CompanyLake
-import org.bson.types.ObjectId
 
 interface CompanyLakeRepository: BaseRepository<CompanyLake>
